28233f164b9e2b8663f1abab51d32bb710c87b0a
BenoitMarechal <benoit.marechal.56@gmail.com>
2023-11-21 12:47:11 +0100

    Integration tests OK

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/IntegrationTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/IntegrationTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Localization;
+using NuGet.ContentModel;
 using P3AddNewFunctionalityDotNetCore.Controllers;
 using P3AddNewFunctionalityDotNetCore.Data;
 using P3AddNewFunctionalityDotNetCore.Models;
@@ -24,28 +25,56 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         private readonly IConfiguration _configuration;
         private readonly IStringLocalizer<ProductService> _localizer;
         [Fact]
-        public void SaveNewProduct()
+        public async Task SaveNewProduct()
         {
             //Arrange        
             var options = new DbContextOptionsBuilder<P3Referential>()
             .UseSqlServer("Server=localhost\\SQLEXPRESS;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
             P3Referential ctx = new (options, _configuration);
-
             LanguageService languageService = new();
             Cart cart = new ();
             ProductRepository productRepository = new (ctx);
             OrderRepository orderRepository = new(ctx);
             ProductService productService= new (cart, productRepository, orderRepository, _localizer);
             ProductController productController = new (productService, languageService);
-            ProductViewModel productViewModel = new() { Id = 10, Name = "Name", Description = "Description ", Details = "Detail", Stock = "1", Price = "150 "};
+            ProductViewModel productViewModel = new() {  Name = "Product from CREATE integration test", Description = "Description ", Details = "Detail", Stock = "1", Price = "150 "};
+            int count = await  ctx.Product.CountAsync();
             //Act
-           Product product =new(){ Id=10, Name = "Name",Description = "Description ", Details = "Detail",Quantity = 1,Price = 150};
-           productRepository.SaveProduct(product);
+            productController.Create(productViewModel);
             //Assert
-           Assert.NotNull(product);
-
+            ////Product Count has been upped by 1
+            Assert.Equal(count+1, ctx.Product.Count());
+            ////Product can be found
+            var product = await ctx.Product.Where(x => x.Name == "Product from CREATE integration test").FirstOrDefaultAsync();
+            Assert.NotNull(product);
+            //END: Clean DATABASE
+            ctx.Product.Remove(product);
+            await ctx.SaveChangesAsync();
+        } 
+        [Fact]
+        public async Task DeleteProduct()
+        {
+            //Arrange        
+            var options = new DbContextOptionsBuilder<P3Referential>()
+            .UseSqlServer("Server=localhost\\SQLEXPRESS;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
+            P3Referential ctx = new(options, _configuration);
+            LanguageService languageService = new();
+            Cart cart = new();
+            ProductRepository productRepository = new(ctx);
+            OrderRepository orderRepository = new(ctx);
+            ProductService productService = new(cart, productRepository, orderRepository, _localizer);
+            ProductController productController = new(productService, languageService);
+            ProductViewModel productViewModel = new() { Name = "Product from DELETE integration test", Description = "Description ", Details = "Detail", Stock = "1", Price = "150 " };
+            int count = await ctx.Product.CountAsync();
+            productController.Create(productViewModel);
+            var product = await ctx.Product.Where(x => x.Name == "Product from DELETE integration test").FirstOrDefaultAsync();
+            //Act
+            ctx.Product.Remove(product);
+            await ctx.SaveChangesAsync();         
+            //Assert
+            Assert.Equal(count , ctx.Product.Count());
+           
         }
-
 
     }
 }
